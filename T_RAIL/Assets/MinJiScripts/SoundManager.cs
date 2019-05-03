66dbbb2c7569f0aeb47bdf64e07a4adef3136586
정민지 <jmj627a@gmail.com>
2019-05-03 09:07:23 +0900

    사운드 추가중인데 어칼지 잘모르겟다...!

---
+++ T_RAIL/Assets/MinJiScripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+
+    enum SoundList
+    {
+        buttonClickSound = 0,
+        stage1BGM = 1
+    }
+
+    enum AudioSourceList
+    {
+        lobbyScene = 0,
+        mainScene = 1
+    }
+
+    public AudioSource[] audioSources;
+    public AudioClip[] sounds;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+
+    public void onButtonClickSound()
+    {
+        //sounds[]
+        //audioSources[SoundList.buttonClickSound] = 
+    }
+}
