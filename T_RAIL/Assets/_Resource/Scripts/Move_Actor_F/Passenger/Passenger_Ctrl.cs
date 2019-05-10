c23f854d1aa3bdbe63edc91d6a2b94f48a7f8d3f
Park Dae Ryeol <spj0123zggg@naver.com>
2019-05-11 01:40:31 +0900

    담벼락 추가

---
+++ T_RAIL/Assets/_Resource/Scripts/Move_Actor_F/Passenger/Passenger_Ctrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Passenger_Ctrl : MonoBehaviour {
+
+
+    Passenger_Actor pass;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // 근데 gamemanager밑에 child로 넣으면 얘도 dontdestroy를 따로 해줘야 될 필요가 있나?
+        pass = new Passenger_Actor();
+    }
+
+  
+    public void Passenger_Add()
+    {
+        // 승객 추가
+
+    }
+
+
+
+
+    // 승객 추가 
+    // 1. 역에서 승객이 보인다.(여기저기 서있음) -> 승객을 클릭하면 태울거냐고 Ui 뜸
+    // 2. 
+}
