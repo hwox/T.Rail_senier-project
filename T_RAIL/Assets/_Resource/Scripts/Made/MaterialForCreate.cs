af8c347664f7d7f43cc92609e1799c43914328c2
Park Hye Won <v_vpollo@naver.com>
2019-07-25 03:01:18 +0900

    아이템 제작창고에 드래그 + 겹치는 숫자 카운트 올라가기 + 박스에서 아이템 빠지면 delete

--- T_RAIL/Assets/_Resource/Scripts/Made/MaterialForCreate.cs
+++ T_RAIL/Assets/_Resource/Scripts/Made/MaterialForCreate.cs
@@ -16,19 +16,27 @@ public class MaterialForCreate : MonoBehaviour
     AllItem_Ctrl allitem;
 
     GameObject[] Storage;
+    int[] WhatInStorage; // 뭐가 자꾸 늘어나네 얘는 그 인덱스에 있는게 몇번째 아이템인ㄴ지
+    Text[] StorageCount;
 
+    // Dictionary<int, int> Storage_test = new Dictionary<int, int>();
+    // 앞에는 index 뒤에는 count
 
+    int StorageIndex = 0;
 
     // Use this for initialization
     void Start()
     {
         allitem = TrainGameManager.instance.allitemCtrl;
         Storage = new GameObject[6];
+        StorageCount = new Text[6];
+        WhatInStorage = new int[6];
         ItemCount = new int[(int)GameValue.itemCategory.woodboard];
 
         for (int i = 0; i < 6; i++)
         {
             Storage[i] = gameObject.transform.GetChild(i).GetChild(0).gameObject;
+            StorageCount[i] = gameObject.transform.GetChild(i).GetChild(1).GetChild(0).GetComponent<Text>();
         }
     }
 
@@ -63,19 +71,27 @@ public class MaterialForCreate : MonoBehaviour
         if (other.CompareTag("DragItem"))
         {
 
-            //ForMakeItem.Add(other.GetComponent<PlayerHand_Item>().NowHave);
-            if (allitem.UseInLeftHand == 1)
+            if (StorageIndex <= 6)
             {
-                ForMakeItem.Add(allitem.LeftHand_Pocket);
-                allitem.UseLeftHandItem();
-            }
-            if (allitem.UseInRightHand == 1)
-            {
-                ForMakeItem.Add(allitem.RightHand_Pocket);
-                allitem.UseRightHandItem();
+                //ForMakeItem.Add(other.GetComponent<PlayerHand_Item>().NowHave);
+                if (allitem.UseInLeftHand == 1)
+                {
+                    ForMakeItem.Add(allitem.LeftHand_Pocket);
+                    allitem.UseLeftHandItem();
+                }
+                if (allitem.UseInRightHand == 1)
+                {
+                    ForMakeItem.Add(allitem.RightHand_Pocket);
+                    allitem.UseRightHandItem();
+                }
+
+                DrawMaterialStorageImage();
             }
 
-            DrawMaterialStorageImage();
+            else
+            {
+                Debug.Log("보관함 6개 넘어서 못넣어 이제");
+            }
         }
     }
 
@@ -83,31 +99,102 @@ public class MaterialForCreate : MonoBehaviour
 
     void DrawMaterialStorageImage()
     {
-        int StorageIndex = 0;
+        //int StorageIndex = 0;
+        bool Enable = false;
+        int index = 0;
 
-        Storage[StorageIndex].GetComponent<Image>().sprite = allitem.ItemImage[ForMakeItem[0] - 1];
-        ItemCount[ForMakeItem[0] - 1] += 1;
+        // 
+        // ItemCount에는 몇개 있는지.
 
-        StorageIndex += 1;
 
-        for (int i = 1; i < ForMakeItem.Count; i++)
+        for (int i = 0; i < 6; i++)
         {
-            // 정렬을 사용하려고 했으나 그러면 맨 먼저 들어왔던 이미지가 걔보다 enum값이 클 경우
-            // 부자연스럽게 뒤로 밀릴 수 있어서 그렇게 안하기로함
-            if (ItemCount[ForMakeItem[i] - 1].Equals(0))
+
+            if (WhatInStorage[i].Equals(ForMakeItem[ForMakeItem.Count - 1]))
             {
 
-                //0 이면 이전에 등록이 안됐다는 소리
-                Storage[StorageIndex].GetComponent<Image>().sprite = allitem.ItemImage[ForMakeItem[0] - 1];
-                StorageIndex += 1;
+                // count-1값이 제일 마지막에 들어온 값일거니끼ㅏ
+                Enable = true;
+                index = i;
+                break;
             }
 
-            ItemCount[ForMakeItem[0] - 1] += 1;
+        }
+        ItemCount[ForMakeItem[ForMakeItem.Count - 1] - 1] += 1;
+
+        if (Enable)
+        {
+            // 이거는 겹치는게 있다는거 
+            // 그러니까 count 값 하나 증가시키고 
+            // text
+            StorageCount[index].text = ItemCount[ForMakeItem[ForMakeItem.Count - 1] - 1].ToString();
         }
 
-        for (int i = StorageIndex; i < 6; i++)
+        else if (!Enable)
+        {
+            // 겹치는거없어
+            // 이미지 띄우기
+            WhatInStorage[StorageIndex] = ForMakeItem[ForMakeItem.Count - 1];
+            Storage[StorageIndex].GetComponent<Image>().sprite = allitem.ItemImage[ForMakeItem[ForMakeItem.Count - 1] - 1];
+            StorageCount[index].text = ItemCount[ForMakeItem[ForMakeItem.Count - 1] - 1].ToString();
+            StorageIndex += 1;
+        }
+
+
+        for (int j = StorageIndex; j < 6; j++)
         {
             Storage[StorageIndex].GetComponent<Image>().sprite = allitem.NullImage;
+
         }
+        //for (int i = 0; i < ForMakeItem.Count; i++)
+        //{
+        //    // 정렬을 사용하려고 했으나 그러면 맨 먼저 들어왔던 이미지가 걔보다 enum값이 클 경우
+        //    // 부자연스럽게 뒤로 밀릴 수 있어서 그렇게 안하기로함
+
+        //    if (i != 0 && ItemCount[ForMakeItem[i] - 1].Equals(0))
+        //    {
+
+        //        //0 이면 이전에 등록이 안됐다는 소리
+        //        Storage[StorageIndex].GetComponent<Image>().sprite = allitem.ItemImage[ForMakeItem[i] - 1];
+        //        StorageIndex += 1;
+        //        ItemCount[ForMakeItem[i] - 1] += 1;
+        //        Debug.Log("1"+ItemCount[ForMakeItem[i] - 1]);
+        //        StorageCount[StorageIndex].text = ItemCount[ForMakeItem[i] - 1].ToString();
+        //    }
+        //    else if (i == 0 && ForMakeItem[0] != 0)
+        //    {
+        //        Storage[StorageIndex].GetComponent<Image>().sprite = allitem.ItemImage[ForMakeItem[0] - 1];
+        //        ItemCount[ForMakeItem[0] - 1] += 1;
+        //        StorageCount[0].text = ItemCount[ForMakeItem[0] - 1].ToString();
+        //        Debug.Log("2" + ItemCount[ForMakeItem[i] - 1]);
+        //        StorageIndex += 1;
+
+        //    }
+        //    else
+        //    {
+        //        // 증가가 됐었음
+        //        // 인덱스 증가할 필요없고
+        //        // Count 증가해야함
+        //        //text 다시
+        //        ItemCount[ForMakeItem[i] - 1] += 1;
+        //        StorageCount[StorageIndex].text = ItemCount[ForMakeItem[i] - 1].ToString();
+        //        Debug.Log("3" + ItemCount[ForMakeItem[i] - 1]);
+        //    }
+
+        //}
+
+
+        //for (int j = StorageIndex; j < 6; j++)
+        //{
+        //    Storage[StorageIndex].GetComponent<Image>().sprite = allitem.NullImage;
+
+        //}
+        //for(int a = 0; a < 8; a++)
+        //{
+        //    // 미친듯한 for문ㅅ ㅏ용...
+        //    ItemCount[a] = 0;
+        //}
+
     }
+
 }
