36b489e5f82d803649fb41a6d3cf600905761465
정민지 <jmj627a@gmail.com>
2019-08-02 16:03:01 +0900

    멀티일때 몬스터 날라감

--- T_RAIL/Assets/DRyeolScripts/ChangeLight.cs
+++ T_RAIL/Assets/DRyeolScripts/ChangeLight.cs
@@ -58,7 +58,7 @@ public class ChangeLight : MonoBehaviour {
         {
             double i = TrainGameManager.instance.runmeter / GameValue.NextStationMeter;
 
-            Debug.Log(i);
+            //Debug.Log(i);
 
             li.color = Color.Lerp(Light_A, Light_B, (float)i);
            
