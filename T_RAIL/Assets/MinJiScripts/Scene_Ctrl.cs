42b6337dd963f116c1203f968ecbc86c4663edc2
정민지 <jmj627a@gmail.com>
2019-08-05 03:42:38 +0900

    디펜스 스텟, 닭 속도 랜덤 동기화

--- T_RAIL/Assets/MinJiScripts/Scene_Ctrl.cs
+++ T_RAIL/Assets/MinJiScripts/Scene_Ctrl.cs
@@ -163,11 +163,12 @@ public class Scene_Ctrl : MonoBehaviourPunCallbacks
             playerListController.playerList[i].MCam_Ctrl.EnemyAppear_Cam(false, 0);
             playerListController.playerList[i].MCam_Ctrl.inTrain();
             //playerListController.playerList[i].stair_down = true;
-            TrainGameManager.instance.TrainCtrl.trainscript[playerListController.playerList[i].player.Where_Train - 1].Ceiling_OnOff(false);
+
+            if(playerListController.playerList[i].player.Where_Train - 1 >= 0)
+                 TrainGameManager.instance.TrainCtrl.trainscript[playerListController.playerList[i].player.Where_Train - 1].Ceiling_OnOff(false);
             
             playerListController.playerList[i].player.Where_Floor = 4;
             playerListController.playerList[i].player.AxeActive();
-            //Debug.Log("id : " + (i) + "  floor : " + playerListController.playerList[i].player.Where_Floor);
 
             //photonView.RPC("setPlayerInStationState", RpcTarget.All, i);
         }
