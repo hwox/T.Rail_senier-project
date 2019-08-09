8ce0068fdb909d82e0f8405cfe086238ab737f42
Park Dae Ryeol <spj0123zggg@naver.com>
2019-08-09 16:34:49 +0900

    엔딩 나가기 수정

--- T_RAIL/Assets/DRyeolScripts/EndingScensTrainSpawn.cs
+++ T_RAIL/Assets/DRyeolScripts/EndingScensTrainSpawn.cs
@@ -39,7 +39,7 @@ public class EndingScensTrainSpawn : MonoBehaviour {
 
     IEnumerator ExitGame()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(15.0f);
 
         while (true)
         {
